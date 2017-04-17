5e9fb1ba529270056d268023bd7b0c878bc5c1d9
proelior <te.proelior@gmail.com>
2017-04-17 09:45:02 +0100

    Update 04-sickrage.sh

--- 04-sickrage.sh
+++ 04-sickrage.sh
@@ -2,7 +2,7 @@
 source vars
 
 ## INFO
-# This script installs and configures sickrage
+# This script installs and configures medusa
 ##
 
 #######################
@@ -16,43 +16,46 @@ fi
 #######################
 # Dependencies
 #######################
-apt-get install -y unrar-free git-core openssl libssl-dev python2.7
+apt-get update && sudo apt-get install unrar-free git-core openssl libssl-dev python2.7
 
 #######################
 # Install
 #######################
-git clone https://github.com/SickRage/SickRage.git /opt/sickrage/
+mkdir /opt/medusa && sudo chown $username:$username /opt/medusa
+git clone https://github.com/pymedusa/Medusa.git /opt/medusa
 
-# Run SickRage for the first time to create default config files
-timeout 5s python /opt/sickrage/SickBeard.py
+# Run Medusa for the first time to create default config files
+timeout 5s python /opt/medusa/SickBeard.py
+
+sleep 5s
 
 #######################
 # Configure
 #######################
-sed -i "s/^tv_download_dir =.*/tv_download_dir = \/home\/$username\/nzbget\/completed\/tv/g" /opt/sickrage/config.ini
-sed -i "s/^root_dirs =.*/root_dirs = 0|\/home\/$username\/$overlayfuse\/tv/g" /opt/sickrage/config.ini
-sed -i "s|naming_pattern =.*|naming_pattern = Season %0S\\\%S_N-S%0SE%0E-%E_N-%Q_N|g" /opt/sickrage/config.ini
+sed -i "s/^tv_download_dir =.*/tv_download_dir = \/home\/$username\/nzbget\/completed\/tv/g" /opt/medusa/config.ini
+sed -i "s/^root_dirs =.*/root_dirs = 0|\/home\/$username\/$overlayfuse\/tv/g" /opt/medusa/config.ini
+sed -i "s|naming_pattern =.*|naming_pattern = Season %0S\\\%S_N-S%0SE%0E-%E_N-%Q_N|g" /opt/medusa/config.ini
 
-sed -i "s/^web_username =.*/web_username = $username/g" /opt/sickrage/config.ini
-sed -i "s/^web_password =.*/web_password = $passwd/g" /opt/sickrage/config.ini
+sed -i "s/^web_username =.*/web_username = $username/g" /opt/medusa/config.ini
+sed -i "s/^web_password =.*/web_password = $passwd/g" /opt/medusa/config.ini
 
-sed -i "s/^nzbget_username =.*/nzbget_username = $username/g" /opt/sickrage/config.ini
-sed -i "s/^nzbget_password =.*/nzbget_password = $passwd/g" /opt/sickrage/config.ini
-sed -i "s/^nzbget_host =.*/nzbget_host = localhost:6789/g" /opt/sickrage/config.ini
+sed -i "s/^nzbget_username =.*/nzbget_username = $username/g" /opt/medusa/config.ini
+sed -i "s/^nzbget_password =.*/nzbget_password = $passwd/g" /opt/medusa/config.ini
+sed -i "s/^nzbget_host =.*/nzbget_host = localhost:6789/g" /opt/medusa/config.ini
 
-sed -i "s/^use_nzbs =.*/use_nzbs = 1/g" /opt/sickrage/config.ini
-sed -i "s/^nzb_method =.*/nzb_method = nzbget/g" /opt/sickrage/config.ini
+sed -i "s/^use_nzbs =.*/use_nzbs = 1/g" /opt/medusa/config.ini
+sed -i "s/^nzb_method =.*/nzb_method = nzbget/g" /opt/medusa/config.ini
 
-sed -i "s/^opensubtitles_password =.*/opensubtitles_password = $openSubtitlesPassword/g" /opt/sickrage/config.ini
-sed -i "s/^opensubtitles_username =.*/opensubtitles_username = $openSubtitlesUsername/g" /opt/sickrage/config.ini
-sed -i "s/^subtitles_languages =.*/subtitles_languages = $openSubtitlesLang/g" /opt/sickrage/config.ini
+sed -i "s/^opensubtitles_password =.*/opensubtitles_password = $openSubtitlesPassword/g" /opt/medusa/config.ini
+sed -i "s/^opensubtitles_username =.*/opensubtitles_username = $openSubtitlesUsername/g" /opt/medusa/config.ini
+sed -i "s/^subtitles_languages =.*/subtitles_languages = $openSubtitlesLang/g" /opt/medusa/config.ini
 
-sed -i 's/^SUBTITLES_SERVICES_LIST =.*/SUBTITLES_SERVICES_LIST = "opensubtitles,addic7ed,legendastv,shooter,subscenter,thesubdb,tvsubtitles"/g' /opt/sickrage/config.ini
-sed -i "s/^use_subtitles =.*/use_subtitles = 1/g" /opt/sickrage/config.ini
-sed -i 's/^SUBTITLES_SERVICES_ENABLED =.*/SUBTITLES_SERVICES_ENABLED = 1|0|0|0|0|0|0|0|0/g' /opt/sickrage/config.ini
+sed -i 's/^SUBTITLES_SERVICES_LIST =.*/SUBTITLES_SERVICES_LIST = "opensubtitles,addic7ed,legendastv,shooter,subscenter,thesubdb,tvsubtitles"/g' /opt/medusa/config.ini
+sed -i "s/^use_subtitles =.*/use_subtitles = 1/g" /opt/medusa/config.ini
+sed -i 's/^SUBTITLES_SERVICES_ENABLED =.*/SUBTITLES_SERVICES_ENABLED = 1|0|0|0|0|0|0|0|0/g' /opt/medusa/config.ini
 
-sed -i "s/^use_failed_downloads =.*/use_failed_downloads = 1/g" /opt/sickrage/config.ini
-sed -i "s/^delete_failed =.*/delete_failed = 1/g" /opt/sickrage/config.ini
+sed -i "s/^use_failed_downloads =.*/use_failed_downloads = 1/g" /opt/medusa/config.ini
+sed -i "s/^delete_failed =.*/delete_failed = 1/g" /opt/medusa/config.ini
 
 ## Post-Processing
 # nzbget
@@ -115,11 +118,14 @@ tee "/home/$username/nzbget/scripts/uploadTV.sh" > /dev/null <<EOF
 
 # Rclone upload to Amazon Cloud Drive
 
+#Set variables
+today=$(date +%Y-%m-%d)
+
 # Wait for NZBget/Sickrage to finish moving files
 sleep 10s
 
 # Upload
-rclone move -c /home/$username/$local/tv $encrypted:tv
+rclone move -c -v --log-file=/home/$username/scripts/logs/uploadACD.${today}.log /home/$username/$local/tv $encrypted:tv
 
 # Tell Plex to update the Library
 wget http://localhost:32400/library/sections/$tvID/refresh?X-Plex-Token=$token
@@ -131,9 +137,9 @@ EOF
 #######################
 # Systemd Service File
 #######################
-tee "/etc/systemd/system/sickrage.service" > /dev/null <<EOF
+tee "/etc/systemd/system/medusa.service" > /dev/null <<EOF
 [Unit]
-Description=SickRage Daemon
+Description=Medusa Daemon
 After=rcloneMount.service
 
 [Service]
@@ -142,7 +148,7 @@ Group=$username
 
 Type=forking
 GuessMainPID=no
-ExecStart=/usr/bin/python2.7 /opt/sickrage/SickBeard.py -q --daemon --nolaunch --datadir=/opt/sickrage
+ExecStart=/usr/bin/python2.7 /opt/medusa/start.py -q --daemon --nolaunch --datadir=/opt/medusa
 
 [Install]
 WantedBy=multi-user.target
@@ -152,30 +158,30 @@ EOF
 # Permissions
 #######################
 chown -R $username:$username /home/$username/$local/tv
-chown -R $username:$username /opt/sickrage
+chown -R $username:$username /opt/medusa
 chmod +x /home/$username/nzbget/scripts/uploadTV.sh
-chown root:root /etc/systemd/system/sickrage.service
-chmod 644 /etc/systemd/system/sickrage.service
+chown root:root /etc/systemd/system/medusa.service
+chmod 644 /etc/systemd/system/medusa.service
 
 #######################
 # Autostart
 #######################
 systemctl daemon-reload
-systemctl start sickrage
-systemctl enable sickrage
+systemctl start medusa
+systemctl enable medusa
 
 #######################
 # Remote Access
 #######################
 echo ''
-echo "Do you want to allow remote access to Sickrage?"
+echo "Do you want to allow remote access to Medusa?"
 echo "If so, you need to tell UFW to open the port."
 echo "Otherwise, you can use SSH port forwarding."
 echo ''
 echo "Would you like us to open the port in UFW?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) ufw allow 8081; echo ''; echo "Port 8081 open, Sickrage is now available over the internet."; echo ''; break;;
+        Yes ) ufw allow 8081; echo ''; echo "Port 8081 open, Medusa is now available over the internet."; echo ''; break;;
         No ) echo "Port 8081 left closed. You can still access it on your local machine by issuing the following command: ssh $username@$ipaddr -L 8081:localhost:8081"; echo "and then open localhost:8081 on your browser."; exit;;
     esac
 done
